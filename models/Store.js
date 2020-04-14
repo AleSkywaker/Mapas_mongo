7152d87048f18ba5414415f1bcae95cc0a51b7a1
Alex Colombo <uricaine@hotmail.com>
2020-04-14 14:26:51 +0200

    finish store model

--- models/Store.js
+++ models/Store.js
@@ -8,4 +8,25 @@ const StoreSchema = new mongoose.Schema({
     trim: true,
     maxlength: [10, 'Store ID must be less than 10 chars'],
   },
+  address: {
+    type: String,
+    required: [true],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = mongoose.model('Store', StoreSchema);
