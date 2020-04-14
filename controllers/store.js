d74c3281cf5c5a6b9e877ce90f748e9722b819d1
Alex Colombo <uricaine@hotmail.com>
2020-04-14 14:26:32 +0200

    finish mwethods to get store and add store

--- controllers/store.js
+++ controllers/store.js
@@ -1,7 +1,39 @@
+const Store = require('../models/store');
+
 // @description Get all Stores
 // @route GET /api/v1/stores
 // @access Public
 
-exports.getStores = (req, res, next) => {
-  res.send('Hello');
+exports.getStores = async (req, res, next) => {
+  try {
+    const stores = await Store.find();
+
+    return res.status(200).json({
+      success: true,
+      count: stores.length,
+      data: stores,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+// @description Create store
+// @route POST /api/v1/stores
+// @access Public
+
+exports.addStore = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const store = await Store.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      data: store,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Server error' });
+  }
 };
